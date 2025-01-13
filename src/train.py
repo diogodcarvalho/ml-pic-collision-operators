697fad78ec7031e611673a39e30481ad5eaca84a
Diogo D. Carvalho <diogo.d.carvalho@tecnico.ulisboa.pt>
2025-01-13 04:31:50 +0000

    fix bug with tmp_folder

--- src/train.py
+++ src/train.py
@@ -140,98 +140,96 @@ def train_standard(cfg, run_id):
 
 def train_temporal_unrolling(cfg, run_id):
 
-    n_stages = len(cfg["temporal_unrolling_stages"])
-    for i_stage, (stage, stage_cfg) in enumerate(
-        cfg["temporal_unrolling_stages"].items()
-    ):
-        print()
-        print(f"Stage: {stage} (#{i_stage+1})")
-        # load train data
-        train_datasets = [
-            TemporalUnrolledDataset(
-                folder=folder,
-                step_size=cfg["dataset"]["step_size"],
-                temporal_unroll_steps=stage_cfg["unrolling_steps"],
-            )
-            for folder in cfg["dataset"]["train"]["folders"]
-        ]
+    with tempfile.TemporaryDirectory() as tmp_dir:
 
-        train_dataset = ConcatDataset(train_datasets)
-
-        train_dataloader = BaseDataLoader(
-            train_dataset,
-            batch_size=len(train_dataset),
-        )
-
-        print("Train Dataset Size:", len(train_dataset))
-
-        # load valid data
-        valid_dataloader = []
-        if cfg["dataset"]["valid"]["folders"] is not None:
-            valid_datasets = [
+        for i_stage, (stage, stage_cfg) in enumerate(
+            cfg["temporal_unrolling_stages"].items()
+        ):
+            print()
+            print(f"Stage: {stage} (#{i_stage+1})")
+            # load train data
+            train_datasets = [
                 TemporalUnrolledDataset(
                     folder=folder,
                     step_size=cfg["dataset"]["step_size"],
                     temporal_unroll_steps=stage_cfg["unrolling_steps"],
                 )
-                for folder in cfg["dataset"]["valid"]["folders"]
+                for folder in cfg["dataset"]["train"]["folders"]
             ]
-            valid_dataset = ConcatDataset(valid_datasets)
-            valid_dataloader = BaseDataLoader(
-                valid_dataset,
-                batch_size=len(valid_dataset),
+
+            train_dataset = ConcatDataset(train_datasets)
+
+            train_dataloader = BaseDataLoader(
+                train_dataset,
+                batch_size=len(train_dataset),
             )
 
-            print("Valid Dataset Size:", len(valid_dataset))
+            print("Train Dataset Size:", len(train_dataset))
 
-        # actions only done in first stage
-        if i_stage == 0:
-            model_kwargs = {
-                "grid_size": train_datasets[0].grid_size,
-                "grid_dx": train_datasets[0].grid_dx,
-            }
+            # load valid data
+            valid_dataloader = []
+            if cfg["dataset"]["valid"]["folders"] is not None:
+                valid_datasets = [
+                    TemporalUnrolledDataset(
+                        folder=folder,
+                        step_size=cfg["dataset"]["step_size"],
+                        temporal_unroll_steps=stage_cfg["unrolling_steps"],
+                    )
+                    for folder in cfg["dataset"]["valid"]["folders"]
+                ]
+                valid_dataset = ConcatDataset(valid_datasets)
+                valid_dataloader = BaseDataLoader(
+                    valid_dataset,
+                    batch_size=len(valid_dataset),
+                )
 
-            model = FokkerPlanck2D(**model_kwargs)
-            mlflow.log_params({"model_kwargs": model_kwargs})
+                print("Valid Dataset Size:", len(valid_dataset))
 
-            print("Model:", model)
+            # actions only done in first stage
+            if i_stage == 0:
+                model_kwargs = {
+                    "grid_size": train_datasets[0].grid_size,
+                    "grid_dx": train_datasets[0].grid_dx,
+                }
 
-            # initialize optimizer
-            optim = optax.adam(float(cfg["optimizer"]["learning_rate"]))
+                model = FokkerPlanck2D(**model_kwargs)
+                mlflow.log_params({"model_kwargs": model_kwargs})
 
-            # only model jax arrays are optimized
-            opt_state = optim.init(eqx.filter(model, eqx.is_array))
+                print("Model:", model)
 
-        # aux functions
-        def loss_fn(model: eqx.Module, x: jax.Array, y: jax.Array):
-            loss = 0
-            y_pred = x.copy()
-            for i in range(stage_cfg["unrolling_steps"]):
-                y_pred = jax.vmap(model)(y_pred)
-                loss += jnp.mean(jnp.square(y_pred - y[:, i])) / n_stages
-            return loss
+                # initialize optimizer
+                optim = optax.adam(float(cfg["optimizer"]["learning_rate"]))
 
-        @eqx.filter_jit
-        def train_step(
-            model: eqx.Module, opt_state: PyTree, x: jax.Array, y: jax.Array
-        ):
-            train_loss, grads = eqx.filter_value_and_grad(loss_fn)(model, x, y)
-            updates, opt_state = optim.update(
-                grads, opt_state, eqx.filter(model, eqx.is_array)
-            )
-            model = eqx.apply_updates(model, updates)
-            return model, opt_state, train_loss
+                # only model jax arrays are optimized
+                opt_state = optim.init(eqx.filter(model, eqx.is_array))
 
-        @eqx.filter_jit
-        def valid_step(model: eqx.Module, x: jax.Array, y: jax.Array):
-            valid_loss = loss_fn(model, x, y)
-            return valid_loss
+            # aux functions
+            def loss_fn(model: eqx.Module, x: jax.Array, y: jax.Array):
+                loss = 0
+                y_pred = x.copy()
+                for i in range(stage_cfg["unrolling_steps"]):
+                    y_pred = jax.vmap(model)(y_pred)
+                    loss += jnp.mean(jnp.square(y_pred - y[:, i]))
+                return loss
 
-        # start training
-        min_loss = jnp.inf
+            @eqx.filter_jit
+            def train_step(
+                model: eqx.Module, opt_state: PyTree, x: jax.Array, y: jax.Array
+            ):
+                train_loss, grads = eqx.filter_value_and_grad(loss_fn)(model, x, y)
+                updates, opt_state = optim.update(
+                    grads, opt_state, eqx.filter(model, eqx.is_array)
+                )
+                model = eqx.apply_updates(model, updates)
+                return model, opt_state, train_loss
 
-        with tempfile.TemporaryDirectory() as tmp_dir:
+            @eqx.filter_jit
+            def valid_step(model: eqx.Module, x: jax.Array, y: jax.Array):
+                valid_loss = loss_fn(model, x, y)
+                return valid_loss
 
+            # start training
+            min_loss = jnp.inf
             if i_stage == 0:
                 step = 0
                 epoch = 0
@@ -276,25 +274,25 @@ def train_temporal_unrolling(cfg, run_id):
                         model.plot(model_img)
                         mlflow.log_artifact(model_img, artifact_path="model_img")
 
-        if cfg["callbacks"] is None:
-            continue
+            if cfg["callbacks"] is None:
+                continue
 
-        if "plot_model_stage" in cfg["callbacks"]:
+            if "plot_model_stage" in cfg["callbacks"]:
+                if "log_model_best" in cfg["callbacks"]:
+                    model = load_equinox_model(run_id, type(model), "weights-best.eqx")
+                model_img = os.path.join(tmp_dir, f"model-stage-{stage}.png")
+                model.plot(model_img)
+                mlflow.log_artifact(model_img, artifact_path="model_img")
+
+        if cfg["callbacks"] is None:
+            return
+
+        if "plot_model_end" in cfg["callbacks"]:
             if "log_model_best" in cfg["callbacks"]:
                 model = load_equinox_model(run_id, type(model), "weights-best.eqx")
-            model_img = os.path.join(tmp_dir, f"model-stage-{stage}.png")
+            model_img = os.path.join(tmp_dir, f"model-final.png")
             model.plot(model_img)
             mlflow.log_artifact(model_img, artifact_path="model_img")
-
-    if cfg["callbacks"] is None:
-        return
-
-    if "plot_model_end" in cfg["callbacks"]:
-        if "log_model_best" in cfg["callbacks"]:
-            model = load_equinox_model(run_id, type(model), "weights-best.eqx")
-        model_img = os.path.join(tmp_dir, f"model-final.png")
-        model.plot(model_img)
-        mlflow.log_artifact(model_img, artifact_path="model_img")
 
 
 def train(cfg, run_id):
