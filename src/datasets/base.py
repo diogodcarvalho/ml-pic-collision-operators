major changes to normalization to improve train stability -> kenels are now correct!

src/datasets/base.py
@@ -51,7 +51,7 @@ class BaseDataset(Dataset):
         i += self.info["i_start"]
         data = np.load(self.folder / f"{i:06d}.npy")
         if normalized:
-            data *= np.prod(self.grid_dx) / self.info["n_samples"]
+            data /= self.info["n_samples"]
         return data
 
     def __len__(self) -> int:

src/models/fokker_planck_2D_base.py
@@ -28,12 +28,12 @@ class FokkerPlanck2DBase(eqx.Module):
         self.ensure_non_negative_f = ensure_non_negative_f
 
     def _grad(self, f: jax.Array, axis: int) -> jax.Array:
-        return (jnp.roll(f, -1, axis) - jnp.roll(f, 1, axis)) / (2 * self.dx[axis])
+        # note: division by dx not used to avoid problems with numerical precision
+        return (jnp.roll(f, -1, axis) - jnp.roll(f, 1, axis)) / 2.0
 
     def _grad2(self, f: jax.Array, axis: int) -> jax.Array:
-        return (jnp.roll(f, -1, axis) - 2 * f + jnp.roll(f, 1, axis)) / (
-            self.dx[axis] ** 2
-        )
+        # note: division by dx^2 not used to avoid problems with numerical precision
+        return jnp.roll(f, -1, axis) - 2 * f + jnp.roll(f, 1, axis)
 
     @property
     def A_grid(self) -> jax.Array:
@@ -64,8 +64,11 @@ class FokkerPlanck2DBase(eqx.Module):
         # Collect all axes
         ax = [ax0, ax1, ax2, ax3, ax4]
 
-        A_grid = self.A_grid
-        B_grid = self.B_grid
+        # Here we multiply by the resolution so that A/B have the right units
+        A_grid = self.A_grid * np.array(self.dx).reshape((2, 1, 1))
+        B_grid = self.B_grid * np.array(
+            [self.dx[0] ** 2, self.dx[1] ** 2, np.prod(self.dx)]
+        ).reshape((3, 1, 1))
         Ax = np.array(A_grid[0])
         Ay = np.array(A_grid[1])
         Bxx = np.array(B_grid[0])

src/train.py
@@ -194,7 +194,7 @@ def train_standard(cfg, run_id):
             mlflow.log_artifact(model_img, artifact_path="model_img")
 
 
-def train_temporal_unrolling(cfg, run_id):
+def train_temporal_unrolling(cfg, run_id, mode="accumulated"):
 
     with tempfile.TemporaryDirectory() as tmp_dir:
 
@@ -278,34 +278,64 @@ def train_temporal_unrolling(cfg, run_id):
                     optim.update(eqx.filter(model, eqx.is_array), opt_state)
 
             # aux functions
-            def loss_fn(model: eqx.Module, x: jax.Array, y: jax.Array):
+            if mode == "accumulated":
 
-                def single_step(i, state):
-                    y_pred = jax.vmap(model)(state[0])
+                def loss_fn(model: eqx.Module, x: jax.Array, y: jax.Array):
+
+                    def single_step(i, state):
+                        y_pred = jax.vmap(model)(state[0])
+                        print(y_pred.shape, y.shape)
+                        if cfg["loss_fn"] == "mae":
+                            loss = state[1] + (
+                                jnp.mean(jnp.abs(y_pred - y[:, i]))
+                                / stage_cfg["unrolling_steps"]
+                            )
+                        elif cfg["loss_fn"] == "mse":
+                            loss = state[1] + (
+                                jnp.mean(jnp.squared(y_pred - y[:, i]))
+                                / stage_cfg["unrolling_steps"]
+                            )
+                        return (y_pred, loss)
+
+                    _, loss_data = jax.lax.fori_loop(
+                        0, stage_cfg["unrolling_steps"], single_step, (x.copy(), 0)
+                    )
+
+                    if "reg_first_deriv" in cfg:
+                        loss_reg = cfg["reg_first_deriv"] * model.get_first_deriv_norm()
+                        loss = loss_data + loss_reg
+                    else:
+                        loss_reg = 0
+                        loss = loss_data
+
+                    return loss, (loss_data, loss_reg)
+
+            elif mode == "last":
+
+                def loss_fn(model: eqx.Module, x: jax.Array, y: jax.Array):
+                    print("mode=last")
+
+                    def single_step(i, state):
+                        y_pred = jax.vmap(model)(state)
+                        return y_pred
+
+                    y_pred = jax.lax.fori_loop(
+                        0, stage_cfg["unrolling_steps"], single_step, x.copy()
+                    )
+
                     if cfg["loss_fn"] == "mae":
-                        loss = state[1] + (
-                            jnp.mean(jnp.abs(y_pred - y[:, i]))
-                            / stage_cfg["unrolling_steps"]
-                        )
+                        loss_data = jnp.mean(jnp.abs(y_pred - y[:, -1]))
                     elif cfg["loss_fn"] == "mse":
-                        loss = state[1] + (
-                            jnp.mean(jnp.squared(y_pred - y[:, i]))
-                            / stage_cfg["unrolling_steps"]
-                        )
-                    return (y_pred, loss)
+                        loss_data = jnp.mean(jnp.squared(y_pred - y[:, -1]))
 
-                _, loss_data = jax.lax.fori_loop(
-                    0, stage_cfg["unrolling_steps"], single_step, (x.copy(), 0)
-                )
+                    if "reg_first_deriv" in cfg:
+                        loss_reg = cfg["reg_first_deriv"] * model.get_first_deriv_norm()
+                        loss = loss_data + loss_reg
+                    else:
+                        loss_reg = 0
+                        loss = loss_data
 
-                if "reg_first_deriv" in cfg:
-                    loss_reg = cfg["reg_first_deriv"] * model.get_first_deriv_norm()
-                    loss = loss_data + loss_reg
-                else:
-                    loss_reg = 0
-                    loss = loss_data
-
-                return loss, (loss_data, loss_reg)
+                    return loss, (loss_data, loss_reg)
 
             @eqx.filter_jit
             def train_step(
@@ -427,7 +457,9 @@ def train(cfg, run_id):
     if cfg["mode"] == "standard":
         train_standard(cfg, run_id)
     elif cfg["mode"] == "temporal_unrolling":
-        train_temporal_unrolling(cfg, run_id)
+        train_temporal_unrolling(cfg, run_id, mode="accumulated")
+    elif cfg["mode"] == "temporal_unrolling_last":
+        train_temporal_unrolling(cfg, run_id, mode="last")
 
     plot_loss(run_id)
     try: