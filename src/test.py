b5688724fd77bfbc7ecb0c7ed1c9bce2051686ab
Diogo D. Carvalho <diogo.d.carvalho@tecnico.ulisboa.pt>
2025-03-03 18:58:56 +0000

    added test routine for conditioned models

--- src/test.py
+++ src/test.py
@@ -48,11 +48,11 @@ def plot_comparison(f_true, f_pred, bin_range, save_to=None):
     plt.close()
 
 
-def test_single_step(cfg, model, run_id):
+def test_single_step(cfg, model, run_id, tmp_dir):
     raise NotImplementedError
 
 
-def test_rollout(cfg, model, run_id):
+def test_rollout(cfg, model, run_id, tmp_dir):
 
     # load train data
     test_datasets = [
@@ -68,63 +68,138 @@ def test_rollout(cfg, model, run_id):
             batch_size=1,
         )
 
-        with tempfile.TemporaryDirectory() as tmp_dir:
+        # load t = 0
+        y_true, _ = next(iter(dataloader))
+        y_pred = y_true.clone()
+        plot_comparison(
+            y_true.numpy(),
+            y_pred.numpy(),
+            bin_range=dataset.grid_range,
+            save_to=os.path.join(tmp_dir, f"{0:06d}.png"),
+        )
 
-            # load t = 0
-            y_true, _ = next(iter(dataloader))
-            y_pred = y_true.clone()
-            plot_comparison(
-                y_true.numpy(),
-                y_pred.numpy(),
-                bin_range=dataset.grid_range,
-                save_to=os.path.join(tmp_dir, f"{0:06d}.png"),
-            )
-
-            # do rollout
-            rollout_mse = []
-            for i, (_, y_true) in tqdm(enumerate(dataloader), total=len(dataset)):
-                y_pred = model(y_pred)
-                # error metrics
-                step_mse = torch.mean(torch.square(y_pred - y_true))
-                rollout_mse.append(step_mse.numpy())
-                # log step mse
-                mlflow.log_metric(f"mse_rollout_{i_dataset}_step", step_mse, step=i)
-
-                if cfg["video"]:
-                    # plot frame comparison
-                    plot_comparison(
-                        y_true.numpy(),
-                        y_pred.numpy(),
-                        bin_range=dataset.grid_range,
-                        save_to=os.path.join(tmp_dir, f"{i+1:06d}.png"),
-                    )
-
-            # log average rollout mse
-            mlflow.log_metric(f"mse_rollout_{i_dataset}", np.mean(rollout_mse))
+        # do rollout
+        rollout_mse = []
+        for i, (_, y_true) in tqdm(enumerate(dataloader), total=len(dataset)):
+            y_pred = model(y_pred)
+            # error metrics
+            step_mse = torch.mean(torch.square(y_pred - y_true))
+            rollout_mse.append(step_mse.numpy())
+            # log step mse
+            mlflow.log_metric(f"mse_rollout_{i_dataset}_step", step_mse, step=i)
 
             if cfg["video"]:
-                # generate video
-                video_fname = os.path.join(tmp_dir, f"rollout_{i_dataset}.mp4")
-                command = [
-                    "ffmpeg",
-                    "-framerate",
-                    str(cfg["fps"]),
-                    "-i",
-                    os.path.join(tmp_dir, "%06d.png"),
-                    "-c:v",
-                    "libx264",
-                    "-r",
-                    str(cfg["fps"]),
-                    "-pix_fmt",
-                    "yuv420p",
-                    video_fname,
-                    "-y",
-                ]
+                # plot frame comparison
+                plot_comparison(
+                    y_true.numpy(),
+                    y_pred.numpy(),
+                    bin_range=dataset.grid_range,
+                    save_to=os.path.join(tmp_dir, f"{i+1:06d}.png"),
+                )
 
-                print(" ".join(command))
-                subprocess.run(command, check=True, capture_output=True)
+        # log average rollout mse
+        mlflow.log_metric(f"mse_rollout_{i_dataset}", np.mean(rollout_mse))
 
-                mlflow.log_artifact(video_fname, "rollout_videos", run_id=run_id)
+        if cfg["video"]:
+            # generate video
+            video_fname = os.path.join(tmp_dir, f"rollout_{i_dataset}.mp4")
+            command = [
+                "ffmpeg",
+                "-framerate",
+                str(cfg["fps"]),
+                "-i",
+                os.path.join(tmp_dir, "%06d.png"),
+                "-c:v",
+                "libx264",
+                "-r",
+                str(cfg["fps"]),
+                "-pix_fmt",
+                "yuv420p",
+                video_fname,
+                "-y",
+            ]
+
+            print(" ".join(command))
+            subprocess.run(command, check=True, capture_output=True)
+
+            mlflow.log_artifact(video_fname, "rollout_videos", run_id=run_id)
+
+
+def test_rollout_conditioned(cfg, model, run_id, tmp_dir):
+
+    # load train data
+    test_datasets = [
+        TemporalUnrolledwConditionersDataset(
+            folder=f, step_size=cfg["data"]["step_size"], conditioners=c
+        )
+        for f, c in zip(
+            cfg["data"]["test"]["folders"], cfg["data"]["test"]["conditioners"]
+        )
+    ]
+
+    # loop over datasets
+    for i_dataset, dataset in enumerate(test_datasets):
+
+        dataloader = BaseDataLoader(
+            dataset,
+            batch_size=1,
+        )
+
+        # load t = 0
+        y_true, _, c = next(iter(dataloader))
+        y_pred = y_true.clone()
+        plot_comparison(
+            y_true.numpy(),
+            y_pred.numpy(),
+            bin_range=dataset.grid_range,
+            save_to=os.path.join(tmp_dir, f"{0:06d}.png"),
+        )
+
+        # do rollout
+        rollout_mse = []
+        for i, (_, y_true, _) in tqdm(enumerate(dataloader), total=len(dataset)):
+            y_pred = model(y_pred, c)
+            # error metrics
+            step_mse = torch.mean(torch.square(y_pred - y_true))
+            rollout_mse.append(step_mse.numpy())
+            # log step mse
+            mlflow.log_metric(f"mse_rollout_{i_dataset}_step", step_mse, step=i)
+
+            if cfg["video"]:
+                # plot frame comparison
+                plot_comparison(
+                    y_true.numpy(),
+                    y_pred.numpy(),
+                    bin_range=dataset.grid_range,
+                    save_to=os.path.join(tmp_dir, f"{i+1:06d}.png"),
+                )
+
+        # log average rollout mse
+        mlflow.log_metric(f"mse_rollout_{i_dataset}", np.mean(rollout_mse))
+
+        if cfg["video"]:
+            # generate video
+            video_fname = os.path.join(tmp_dir, f"rollout_{i_dataset}.mp4")
+            command = [
+                "ffmpeg",
+                "-framerate",
+                str(cfg["fps"]),
+                "-i",
+                os.path.join(tmp_dir, "%06d.png"),
+                "-c:v",
+                "libx264",
+                "-r",
+                str(cfg["fps"]),
+                "-pix_fmt",
+                "yuv420p",
+                video_fname,
+                "-y",
+            ]
+
+            print(" ".join(command))
+            subprocess.run(command, check=True, capture_output=True)
+
+            mlflow.log_artifact(video_fname, "rollout_videos", run_id=run_id)
 
 
 def test(cfg, run_id):
@@ -165,8 +240,14 @@ def test(cfg, run_id):
     print("model:", model)
 
     with torch.no_grad():
-        if cfg["mode"] == "rollout":
-            test_rollout(cfg, model, run_id)
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            if cfg["mode"] == "rollout":
+                if issubclass(type(model), FokkerPlanck2DBaseConditioned):
+                    print("conditioned")
+                    test_rollout_conditioned(cfg, model, run_id, tmp_dir)
+                else:
+                    print("normal")
+                    test_rollout(cfg, model, run_id, tmp_dir)
 
-        if cfg["mode"] == "single_step":
-            test_single_step(cfg, model, run_id)
+            if cfg["mode"] == "single_step":
+                test_single_step(cfg, model, run_id, tmp_dir)
