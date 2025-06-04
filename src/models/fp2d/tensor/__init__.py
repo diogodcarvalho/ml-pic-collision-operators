55223837c88e8c4693dc600a9711e06dffbed7dd
Diogo D. Carvalho <diogo.d.carvalho@tecnico.ulisboa.pt>
2025-06-04 17:34:26 +0100

    init changes were missing

--- src/models/fp2d/tensor/__init__.py
+++ src/models/fp2d/tensor/__init__.py
@@ -6,3 +6,4 @@ from .AquadBquad import FokkerPlanck2D_AquadBquad
 from .AlineBline import FokkerPlanck2D_AlineBline
 from .ArBquad import FokkerPlanck2D_ArBquad
 from .ArBr import FokkerPlanck2D_ArBr
+from .ABparperp import FokkerPlanck2D_ABparperp
