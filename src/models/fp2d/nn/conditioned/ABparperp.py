better 1D interpolation function for uniform grids + fixes some issues with ABparperp real units cls methods

src/models/fp2d/nn/conditioned/ABparperp.py
@@ -147,7 +147,7 @@ class FokkerPlanck2DNNConditioned_ABparperp(FokkerPlanck2DNNBaseConditioned):
             conditioners, torch.unique(self.vr_grid.detach()).reshape(-1, 1)
         )
         Apar = self.A(inputs).detach().cpu().numpy() * self.grid_dx[0]
-        return Apar
+        return Apar.reshape(conditioners.shape[0], -1)
 
     def Bpar_real(self, conditioners: torch.Tensor) -> torch.Tensor:
         if conditioners.ndim == 1:
@@ -158,7 +158,7 @@ class FokkerPlanck2DNNConditioned_ABparperp(FokkerPlanck2DNNBaseConditioned):
             conditioners, torch.unique(self.vr_grid.detach()).reshape(-1, 1)
         )
         Bpar = self.Bpar(inputs).detach().cpu().numpy() * self.grid_dx[0] ** 2
-        return Bpar
+        return Bpar.reshape(conditioners.shape[0], -1)
 
     def Bperp_real(self, conditioners: torch.Tensor) -> torch.Tensor:
         if conditioners.ndim == 1:
@@ -170,7 +170,7 @@ class FokkerPlanck2DNNConditioned_ABparperp(FokkerPlanck2DNNBaseConditioned):
             torch.unique(self.vr_grid.detach()).reshape(-1, 1),
         )
         Bperp = self.Bperp(inputs).detach().cpu().numpy() * self.grid_dx[0] ** 2
-        return Bperp
+        return Bperp.reshape(conditioners.shape[0], -1)
 
     def A_grid(self, conditioners: torch.Tensor) -> torch.Tensor:
         # (batch_size * grid_size**2, 1 + C)

src/models/fp2d/tensor/conditioned/ABparperp.py
@@ -149,11 +149,9 @@ class FokkerPlanck2DTime_ABparperp(FokkerPlanck2DBaseTime):
                 torch.square(self.Bpar[2:] - 2 * self.Bpar[1:-1] + self.Bpar[:-2])
             )
             + torch.mean(
-                torch.square(
-                    self.Bperp[2:] - 2 * self.Bperp[1:-1] + self.Bperp[:-2]
-                )
+                torch.square(self.Bperp[2:] - 2 * self.Bperp[1:-1] + self.Bperp[:-2])
             )
-            + 1e-10 # have to add to have gradients defined at initialization
+            + 1e-10  # have to add to have gradients defined at initialization
         )
         # time and v
         # return torch.sqrt(

src/models/fp2d/tensor/conditioned/base.py
@@ -115,25 +115,24 @@ class FokkerPlanck2DBaseTime(nn.Module):
         )
 
     def A_grid(self, t: torch.Tensor) -> np.ndarray:
-        A = self._t_interpolate(self.A.reshape(self.n_t, -1), t)
-        return A.reshape(t.shape[0], 2, *self.grid_size)
+        A = self._t_interpolate(self.A, t)
+        return A
 
     def B_grid(self, t: torch.Tensor) -> np.ndarray:
-        B = self._t_interpolate(self.B.reshape(self.B.shape[0], -1), t)
-        return B.reshape(t.shape[0], 3, *self.grid_size)
+        return self._t_interpolate(self.B, t)
 
     def A_grid_real(self, t: torch.Tensor) -> np.ndarray:
-        return np.array(self.A_grid(t).detach().cpu().numpy()[0]) * np.array(
+        return np.array(self.A_grid(t).detach().cpu().numpy()) * np.array(
             self.grid_dx
-        ).reshape((2, 1, 1))
+        ).reshape((1, 2, 1, 1))
 
     def B_grid_real(self, t: torch.Tensor) -> np.ndarray:
-        B = self.B_grid(t).detach().cpu()[0]
+        B = self.B_grid(t).detach().cpu()
         if self.ensure_non_negative_B:
-            B[:2] = torch.clamp(B[:2], min=0)
+            B[:, :2] = torch.clamp(B[:, :2], min=0)
         return np.array(B.numpy()) * np.array(
             [self.grid_dx[0] ** 2, self.grid_dx[1] ** 2, np.prod(self.grid_dx)]
-        ).reshape((3, 1, 1))
+        ).reshape((1, 3, 1, 1))
 
     def change_attribute(self, attr_name: str, attr_value: Any):
         if attr_name in [

src/models/op2d/base_gradient_kxy.py
@@ -79,7 +79,7 @@ class Operator2DBase_Gradient_Kxy(nn.Module):
         elif self.gradient_order == 2:
             return torch.gradient(f, dim=axis)[0]
         else:
-            raise NotImplementedError("gradient_order must be 1 or 2")
+            raise NotImplementedError("gradient_order must be -1, 1 or 2")
 
     def plot(self, save_to: str | None = None):
 