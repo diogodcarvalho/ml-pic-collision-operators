cef525cd9ae632f5a52c552347fdd4a681fd95fe
Diogo D. Carvalho <diogo.d.carvalho@tecnico.ulisboa.pt>
2025-12-20 18:46:01 +0000

    add example data + configs + scripts

---
+++ examples/nn-train.sh
@@ -0,0 +1,9 @@
+# The command follows the form:
+# mlpic_run config_file.yaml \
+#   experiment_name \
+#   run_name \
+#   mlflow_directory
+mlpic_run examples/nn-train.yaml \
+    examples-train \
+    nn-train \
+    mlruns
---
+++ examples/tensor-test.sh
@@ -0,0 +1,9 @@
+# The command follows the form:
+# mlpic_run config_file.yaml \
+#   experiment_name \
+#   run_name \
+#   mlflow_directory
+mlpic_run examples/tensor-test.yaml \
+    examples-test \
+    tensor-test \
+    mlruns
