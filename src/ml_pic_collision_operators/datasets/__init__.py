f7031dc2c5784d3831e844a2c0e23935cb4ad329
Diogo D. Carvalho <diogo.d.carvalho@tecnico.ulisboa.pt>
2026-01-04 15:07:19 +0000

    removed unused OnDeviceDataset

--- src/ml_pic_collision_operators/datasets/__init__.py
+++ src/ml_pic_collision_operators/datasets/__init__.py
@@ -2,4 +2,3 @@ from .base import BaseDataset
 from .base_wconditioners import BasewConditionersDataset
 from .temporal_unrolled import TemporalUnrolledDataset
 from .temporal_unrolled_wconditioners import TemporalUnrolledwConditionersDataset
-from .on_device import OnDeviceDataset
